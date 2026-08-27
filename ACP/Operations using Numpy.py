703c5226cf4806c98d63e7c4818248b4552b412d
emi-alt <zareenomera247@gmail.com>
2026-08-27 20:26:23 +0530

    opr in numpy

--- ACP/Operations using Numpy.py
+++ ACP/Operations using Numpy.py
@@ -1,8 +1,16 @@
 import numpy as np
 num = [0, 1, 2, 3,4 ,5 ,6 ,7 ,8 ,9]
 arr = np.array(num)
-for i in num:
-    if i % 2 != 0:
-        print(i, "is an odd number")
-        new_arr = [-1 for i in arr if i%2 != 0] + [i for i in arr if i%2 == 0]
-print(new_arr)
+print("Original Array: ", arr)
+
+modify = np.where(arr%2 != 0, -1, arr)
+print("Modified Array: ", modify)
+
+twoDarr = np.reshape(arr, (2, -1))
+print("2D Array: ", twoDarr)
+
+even_sum = 0
+for i in arr:
+    if i % 2 == 0:
+        even_sum += i
+print("Sum of Even Numbers: ", even_sum)
